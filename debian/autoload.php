Vitexus/php-phpdocumentor-guides

Add Debian packaging
Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

debian/autoload.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '/usr/share/php/Composer/InstalledVersions.php';
+
+spl_autoload_register(function (string $class): void {
+    $prefix = 'phpDocumentor\\Guides\\';
+    if (str_starts_with($class, $prefix)) {
+        $file = '/usr/share/php/phpDocumentor/Guides/' . str_replace('\\', '/', substr($class, strlen($prefix))) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+    }
+});
+
+(function (): void {
+    $versions = [];
+    foreach (\Composer\InstalledVersions::getAllRawData() as $d) {
+        $versions = array_merge($versions, $d['versions'] ?? []);
+    }
+    $name    = 'unknown';
+    $version = '0.0.0';
+    $versions[$name] = ['pretty_version' => $version, 'version' => $version,
+        'reference' => null, 'type' => 'library', 'install_path' => __DIR__,
+        'aliases' => [], 'dev_requirement' => false];
+    \Composer\InstalledVersions::reload([
+        'root' => ['name' => $name, 'pretty_version' => $version, 'version' => $version,
+            'reference' => null, 'type' => 'library', 'install_path' => __DIR__,
+            'aliases' => [], 'dev' => false],
+        'versions' => $versions,
+    ]);
+})();

Vitexus/php-phpdocumentor-json-path

debian/autoload.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '/usr/share/php/Composer/InstalledVersions.php';
+
+spl_autoload_register(function (string $class): void {
+    $prefix = 'phpDocumentor\\JsonPath\\';
+    if (str_starts_with($class, $prefix)) {
+        $file = '/usr/share/php/phpDocumentor/JsonPath/' . str_replace('\\', '/', substr($class, strlen($prefix))) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+    }
+});
+
+(function (): void {
+    $versions = [];
+    foreach (\Composer\InstalledVersions::getAllRawData() as $d) {
+        $versions = array_merge($versions, $d['versions'] ?? []);
+    }
+    $name    = 'unknown';
+    $version = '0.0.0';
+    $versions[$name] = ['pretty_version' => $version, 'version' => $version,
+        'reference' => null, 'type' => 'library', 'install_path' => __DIR__,
+        'aliases' => [], 'dev_requirement' => false];
+    \Composer\InstalledVersions::reload([
+        'root' => ['name' => $name, 'pretty_version' => $version, 'version' => $version,
+            'reference' => null, 'type' => 'library', 'install_path' => __DIR__,
+            'aliases' => [], 'dev' => false],
+        'versions' => $versions,
+    ]);
+})();